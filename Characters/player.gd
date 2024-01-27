e7c410ba7fbfae6f3bc4672fc6eaf036433fb91d
Perrine Casbas <perrine.casbas@epitech.eu>
2024-01-27 22:43:50 +0100

    Add: death

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,49 +1,30 @@
 extends CharacterBody2D
-
+ 
 @export var move_speed : float = 200
 @export var starting_direction : Vector2 = Vector2(0, 1)
- #parameters/Idle/blend_position
-
+#parameters/Idle/blend_position
+ 
 @onready var animation_tree = $AnimationTree
-
+ 
+var dead : bool = false
 func _ready():
-	update_animation_parameters(starting_direction)
-
+	update_animation_parameters(starting_direction, dead)
+ 
 func _physics_process(_delta):
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
-	update_animation_parameters(input_direction)
+	#if update_animation_parameters(input_direction, dead) == true put dead
+	update_animation_parameters(input_direction, dead)
 	velocity = input_direction * move_speed
 	move_and_slide()
-
-func update_animation_parameters(move_input : Vector2):
+ 
+func update_animation_parameters(move_input : Vector2, is_dead : bool):
+	if dead:
+		#update dead
+		return true
 	if (move_input != Vector2.ZERO):
 		animation_tree.set("parameters/Walk/blend_position", move_input)
 	animation_tree.set("parameters/Idle/blend_position", move_input)
-		
-	
-
-#var input_movement = Vector2.ZERO
-#var move_speed = 100
-#@onready var animation_tree = $AnimationTree
-#var anim_state = animation_tree.get("parameters/playback")
-#var test = animatio_
-#
-#func _physics_process(_delta):
-	#move()
-#
-#func move():
-	#input_movement = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	#if (input_movement != Vector2.ZERO):
-		#animation_tree.set("parameters/Idle/blend_position", input_movement)
-		#animation_tree.set("parameters/Walk/blend_position", input_movement)
-		#velocity = input_movement * move_speed
-#
-	#if (input_movement == Vector2.ZERO):
-		#anim_state.travel("Idle")
-		#velocity = Vector2.ZERO
-	#move_and_slide()
-	#
-		#
+	return true
